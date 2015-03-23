18e464b486ff8f04f65b47276d1b4b342e80b657
manojrammurthy <manrox.drag@gmail.com>
2015-03-23 13:45:04 +0530

    git hub oauth PHP SDK uploaded

---
+++ Github_Lib/githubConfig.php
@@ -0,0 +1,9 @@
+<?php
+/*
+ * All Config necessarry
+ */
+$config = array('client_id' => '',							//  Github client ID
+				'client_secret' => '',	//  Github client SECRET
+				'redirect_url' => 'http://localhost/githublogin',	//  Github Redirect URL (As mentioned in GITHUB APP PAGE)
+				'app_name' => '');								//  Github App Name (As mentioned in GITHUB APP PAGE)
+?>
